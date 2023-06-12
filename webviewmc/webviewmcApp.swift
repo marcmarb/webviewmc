59b565df46ac1636a556a0ef9b208ee8ded5760f
marcmarb <fotsmacc@gmail.com>
2023-06-12 15:48:26 -0600

    Error

---
+++ webviewmc/webviewmcApp.swift
@@ -0,0 +1,31 @@
+//
+//  webviewmcApp.swift
+//  webviewmc
+//
+//  Created by Marcos on 12/06/23.
+//
+
+import UIKit
+import WebKit
+
+
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
